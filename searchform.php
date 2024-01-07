f7b58d40197933041307eaa63aecbc762ef4afba
GO MIKA <wakamiya1019@gmail.com>
2024-01-07 15:19:11 +0900

    1/7

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 
-<form class="search-icon" method="get" action="<?php echo home_url(); ?>/">
-    <input type="search" placeholder="キーワードを入力" class="search" name="s" id="s>
+<form class="search-icon" method="get" action="<?php echo esc_url( home_url() ); ?>/search">
+    <input type="search" value="<?php the_search_query();?>" placeholder="キーワードを入力" class="search" name="s" id="s>
     <i class="fa fa-search"></i>
     <input type="submit" value="検索" class="search-btn">
 </form>
