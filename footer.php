bddfbe277537b81273d88e0dda6c469b76a6b9a4
GO MIKA <wakamiya1019@gmail.com>
2024-01-19 01:34:35 +0900

    IDセレクタ変更

--- footer.php
+++ footer.php
@@ -12,9 +12,6 @@
     <p><small>Copyright: RaiseTech</small></p>
 </footer>
 <?php wp_footer(); ?>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
 </body>
 </html>
 
